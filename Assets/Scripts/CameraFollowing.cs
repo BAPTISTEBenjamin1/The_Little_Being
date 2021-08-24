5c9ba7b5f1874600af76b6056ae96095f88ac193
BAPTISTEBenjamin1 <ben.baptiste64@hotmail.com>
2021-08-24 21:32:53 +0200

    save

--- Assets/Scripts/CameraFollowing.cs
+++ Assets/Scripts/CameraFollowing.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CameraFollowing : MonoBehaviour
 {
-    [SerializeField] Transform m_player;
+    [SerializeField] Transform m_cameraTarget;
+    [SerializeField] float m_smoothSpeed = 0.125f;
+    [SerializeField] Vector3 m_offset;
+    
 
     
     // Start is called before the first frame update
@@ -14,11 +17,14 @@ public class CameraFollowing : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         
-       Vector3 direction = player.position - transform.position;
-       float angle = Mathf.Atan3(direction.y, direction.x, direction.z);
+        Vector3 desiredPosition = m_cameraTarget.position + m_offset;
+        Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, m_smoothSpeed);
+        transform.position = smoothedPosition;
+        transform.rotation = m_cameraTarget.rotation;
 
     }
+    
 }
